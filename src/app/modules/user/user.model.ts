7fb68398d4395d2bfb8a9e67dacf98654aa97f4a
aothymoon59 <aothymoon59@gmail.com>
2024-08-13 00:34:14 +0600

    [ADD] user model created

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,36 @@
+import { model, Schema } from 'mongoose';
+import { TUser } from './user.interface';
+
+const userSchema = new Schema<TUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = model<TUser>('User', userSchema);
