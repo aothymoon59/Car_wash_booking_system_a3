faef3ac6087471e3adf5143b1b9497d5b4f122e5
aothymoon59 <aothymoon59@gmail.com>
2024-08-13 20:55:28 +0600

    [ADD] login user service

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,6 +3,10 @@ import bcrypt from 'bcrypt';
 import config from '../../config';
 import { TUser } from '../user/user.interface';
 import { User } from '../user/user.model';
+import { createToken } from './auth.utils';
+import AppError from '../../errors/AppError';
+import httpStatus from 'http-status';
+import { TLoginUser } from './auth.interface';
 
 const signUpUserIntoDB = async (payload: TUser) => {
   const hashedPassword = await bcrypt.hash(
@@ -15,6 +19,43 @@ const signUpUserIntoDB = async (payload: TUser) => {
   const result = await User.create(payload);
 
   return result;
+};
+
+const loginUser = async (payload: TLoginUser) => {
+  const user = await User.findOne({ email: payload?.email })
+    .select('+password')
+    .select({ createdAt: 0, updatedAt: 0, __v: 0 });
+  // checking if the user is exist
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !');
+  }
+
+  // checking if the user is matched or not
+  const isPasswordMatched = await bcrypt.compare(
+    payload?.password,
+    user?.password,
+  );
+
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.FORBIDDEN, 'Password do not matched !');
+  }
+
+  const jwtPayload = {
+    _id: user._id.toString(),
+    email: user.email,
+    role: user.role,
+  };
+
+  const token = createToken(
+    jwtPayload,
+    config.jwt_access_secret as string,
+    config.jwt_access_expires_in as string,
+  );
+
+  return {
+    user,
+    token,
+  };
 };
 
 export const AuthServices = {
